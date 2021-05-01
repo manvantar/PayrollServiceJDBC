67356877dbfea9acc6a9d816db306917071c9d13
manvantar <manukvshetty@gmail.com>
2021-05-01 14:04:44 +0530

    Retrieve the data from Database and Test

---
+++ src/test/java/com/payrollservicejdbc/EmpPayrollTester.java
@@ -0,0 +1,21 @@
+package com.payrollservicejdbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class EmpPayrollTester {
+    EmployeePayrollService employeePayrollService;
+    List<EmployeePayrollData> employeePayrollList;
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrived_ShouldMatchEmployeeCount() throws SQLException {
+        employeePayrollService = new EmployeePayrollService();
+        employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assertions.assertEquals(3, employeePayrollList.size());
+    }
+
+
+}
